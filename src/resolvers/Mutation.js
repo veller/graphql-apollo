d5284362a0ca9dc8c37f497819c9447e7d64b10e
veller <vellerbauer@gmail.com>
2022-04-17 14:57:28 -0300

    feat: add authentication

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,73 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { APP_SECRET } = require("../utils");
+
+async function post(parent, args, context, info) {
+  const { userId } = context;
+
+  return await context.prisma.link.create({
+    data: {
+      url: args.url,
+      description: args.description,
+      postedBy: { connect: { id: userId } },
+    },
+  });
+}
+
+async function updateLink(parent, args, context, info) {
+  return await context.prisma.link.update({
+    where: { id: Number(args.id) },
+    data: {
+      url: args.url,
+      description: args.description,
+    },
+  });
+}
+
+async function deleteLink(parent, args, context, info) {
+  return await context.prisma.link.delete({
+    where: { id: Number(args.id) },
+  });
+}
+
+async function signup(parent, args, context, info) {
+  const password = await bcrypt.hash(args.password, 10);
+  const user = await context.prisma.user.create({
+    data: { ...args, password },
+  });
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
+}
+
+async function login(parent, args, context, info) {
+  const user = await context.prisma.user.findUnique({
+    where: { email: args.email },
+  });
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) {
+    throw new Error("Invalid password");
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
+}
+
+module.exports = {
+  signup,
+  login,
+  post,
+  updateLink,
+  deleteLink,
+};
